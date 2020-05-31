81b38d65378c6345b2113b1d6a78bb73f974f5a3
Prashant Pokhriyal <pokhariyalp@gmail.com>
2020-06-01 01:19:16 +0530

    add support for parsing lsq-api menus

---
+++ lsq_api_crawler/spiders/apis_spider.py
@@ -0,0 +1,21 @@
+import scrapy
+
+
+class APIsSpider(scrapy.Spider):
+    name = 'apis'
+    start_urls = [
+        'https://apidocs.leadsquared.com'
+    ]
+
+    def parse(self, response):
+        for li in response.css('.bellows-menu-item.bellows-menu-item-type-post_type.bellows-menu-item-object-page.bellows-menu-item-has-children'):
+            yield {
+                'title': li.xpath('.//a/span/text()').get(),
+                'subMenu': list(map(lambda ul: list(map(lambda ul: {
+                    'title': ul.xpath('a/span/text()').get(),
+                    'subMenu': list(map(lambda li: {
+                        'title': li.xpath('span/text()').get(),
+                        'href': li.xpath('@href').get()
+                    }, ul.xpath('ul/li/a'))),
+                }, ul.xpath('li'))), li.xpath('ul'))),
+            }
