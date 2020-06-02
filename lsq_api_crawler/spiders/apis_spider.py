f1eacab3459f35dc72f5d3f8a0c43cf2c9312151
Prashant Pokhriyal <pokhariyalp@gmail.com>
2020-06-03 00:14:27 +0530

    optimize menu crawling

--- lsq_api_crawler/spiders/apis_spider.py
+++ lsq_api_crawler/spiders/apis_spider.py
@@ -11,11 +11,11 @@ class APIsSpider(scrapy.Spider):
         for li in response.css('.bellows-menu-item.bellows-menu-item-type-post_type.bellows-menu-item-object-page.bellows-menu-item-has-children'):
             yield {
                 'title': li.xpath('.//a/span/text()').get(),
-                'subMenu': list(map(lambda ul: list(map(lambda ul: {
+                'subMenu': list(map(lambda ul: {
                     'title': ul.xpath('a/span/text()').get(),
                     'subMenu': list(map(lambda li: {
                         'title': li.xpath('span/text()').get(),
                         'href': li.xpath('@href').get()
                     }, ul.xpath('ul/li/a'))),
-                }, ul.xpath('li'))), li.xpath('ul'))),
+                }, li.xpath('ul/li')))
             }
